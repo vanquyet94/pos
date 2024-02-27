cf62cd66a5bd0b499ab03997d01c48d281fded8b
Wit <vanquyet94@gmail.com>
2024-02-27 08:08:11 +0700

    update auth

--- src/admin/admin-auth/admin-auth.module.ts
+++ src/admin/admin-auth/admin-auth.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AdminAuthResolver } from './admin-auth.resolver';
+import { JwtStrategy } from './guards/jwt-auth-guard';
 
 @Module({
-  providers: [AdminAuthResolver]
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
+  providers: [AdminAuthResolver, JwtStrategy]
 })
-export class AdminAuthModule {}
+export class AdminAuthModule { }
