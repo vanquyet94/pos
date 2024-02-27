c4d97de47d67e2ed81848b99f608439dc09067ab
Wit <vanquyet94@gmail.com>
2024-02-27 08:08:22 +0700

    update auth

---
+++ src/admin/admin-auth/guards/jwt-auth-guard.ts
@@ -0,0 +1,23 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserService } from 'src/modules/user/user.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly _userService: UserService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT,
+    });
+  }
+
+  async validate(payload: any) {
+    const user = await this._userService.findUserById(payload.sub);
+    if (!user || !payload) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
